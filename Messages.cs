6e5b2ac81a151fd0d00551038eb6690b0113a38d
GeorgeCLu <52149356+GeorgeCLu@users.noreply.github.com>
2024-07-30 18:42:16 +1200

    Initial upload
    Initial upload

---
+++ Messages.cs
@@ -0,0 +1,25 @@
+// Messages.cs
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Azure.WebJobs.Extensions.SignalRService;
+
+namespace Company
+{
+  public static class Messages
+  {
+
+    [FunctionName("messages")]
+    public static Task SendMessage(
+          [HttpTrigger(AuthorizationLevel.Anonymous, "post")] object message,
+          [SignalR(HubName = "chat")] IAsyncCollector<SignalRMessage> signalRMessages)
+    {
+      return signalRMessages.AddAsync(
+          new SignalRMessage
+          {
+            Target = "newMessage",
+            Arguments = new[] { message }
+          });
+    }
+  }
+}
